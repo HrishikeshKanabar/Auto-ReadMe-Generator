1dd7bf1a7b39a3f833f36d515f24d8d908b02565
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2021-10-19 20:37:54 -0400

    added more license and logic

--- Develop/index.js
+++ Develop/index.js
@@ -1,90 +1,99 @@
 // Included packages needed for this application
 
-const inq = require('inquirer');
-const fileSys = require('fs');
-const genMarkdown = require('./utils/generateMarkdown.js');
-
-
+const inq = require("inquirer");
+const fileSys = require("fs");
+const genMarkdown = require("./utils/generateMarkdown.js");
 
 // An array of questions for user input
 const questions = [
-    {
-        type:"input",
-        name:"title",
-        message:"What should be title for your project ?(Required)",
-        validate:(title)=>{if(title){return true}else{return 'Title is required to generate "ReadME" file'}},
-
-
+  {
+    type: "input",
+    name: "title",
+    message: "What should be title for your project ?(Required)",
+    validate: (title) => {
+      if (title) {
+        return true;
+      } else {
+        return 'Title is required to generate "ReadME" file';
+      }
     },
-    {
-        type:"input",
-        name:"desc",
-        message:"What should be description for your project ?",
-
-    },   
-    {
-        type:"input",
-        name:"install",
-        message:"Please enter intallation instruction for your project :",
-
-    }, 
-    {
-        type:"input",
-        name:"usage",
-        message:"Please enter usage information about project :",
-
-    },  
-    {
-        type:"input",
-        name:"contri",
-        message:"Please add guidelines for contributing in your project :",
-
-    },
-    {
-        type:"input",
-        name:"test",
-        message:"which test are perfomred for this project ?",
-
-    },
-    {
+  },
+  {
+    type: "input",
+    name: "desc",
+    message: "What should be description for your project ?",
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "Please enter intallation instruction for your project :",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please enter usage information about project :",
+  },
+  {
+    type: "input",
+    name: "contri",
+    message: "Please add guidelines for contributing in your project :",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "which test are perfomred for this project ?",
+  },
+  {
     type: "list",
     message: "What kind of license should your project have?",
     name: "license",
-    choices: ["GPL","MIT", "APACHE","BSD","IPL","None"]
-    }, 
-    {
-        type:"input",
-        name:"username",
-        message:"Please enter your Github username :",
-        validate:(username)=>{if(username){return true}else{return 'Username is required to generate "ReadME" file'}},
+    choices: [
+      "GPLv3",
+      "GPLv2",
+      "MIT",
+      "APACHE 2.0",
+      "BSD 2-Clause",
+      "BSD 3-Clause",
+      "IPL",
+      "None",
+    ],
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "Please enter your Github username :",
+    validate: (username) => {
+      if (username) {
+        return true;
+      } else {
+        return 'Username is required to generate "ReadME" file';
+      }
     },
-    {
-        type:"input",
-        name:"email",
-        message:"Please enter your Github email :",
-        
-    },   
-
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please enter your Github email :",
+  },
 ];
 
 // Function to write README file
 function writeToFile(fileName, data) {
-    fileSys.writeFile(fileName,data,err=>{
-        if(err){
-            return err;
-        }
-        console.log("Your 'README' has generated sucessfully !");
-    });
+  fileSys.writeFile(fileName, data, (err) => {
+    if (err) {
+      return err;
+    }
+    console.log("Your 'README' has generated sucessfully !");
+  });
 }
 
 //Function to initialize app
 function init() {
-
-    inq.prompt(questions).then(data=>{
-        const dataForReadme =genMarkdown(data);
-        console.log(dataForReadme)
-        writeToFile('./genratedReadMe.md',dataForReadme);
-    });
+  inq.prompt(questions).then((data) => {
+    const dataForReadme = genMarkdown(data);
+    console.log(dataForReadme);
+    writeToFile("./genratedReadMe.md", dataForReadme);
+  });
 }
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,52 +1,83 @@
 // A function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(license=='GPL'){
-    return '[![GitHub license](https://img.shields.io/badge/license-'+license+'-red.svg)]';
-  }else if(license=='MIT'){
-    return '[![GitHub license](https://img.shields.io/badge/license-'+license+'-yellow.svg)]'; 
-  }else if(license=='APACHE'){
-    return '[![GitHub license](https://img.shields.io/badge/license-'+license+'-blue.svg)]'; 
-  }else if(license=='BSD'){
-    return '[![GitHub license](https://img.shields.io/badge/license-'+license+'-orange.svg)]'; 
-  }else if(license=='IPL'){
-    return '[![GitHub license](https://img.shields.io/badge/license-'+license+'-brightgreen.svg)]'; 
-  }else{
-    return ' ';
+  if (license == "GPLv3") {
+    return (
+      "[![GitHub license](https://img.shields.io/badge/license-" +
+      license +
+      "-red.svg)]"
+    );
+  } else if (license == "GPLv2") {
+    return (
+      "[![GitHub license](https://img.shields.io/badge/license-" +
+      license +
+      "-red.svg)]"
+    );
+  } else if (license == "MIT") {
+    return (
+      "[![GitHub license](https://img.shields.io/badge/license-" +
+      license +
+      "-yellow.svg)]"
+    );
+  } else if (license == "APACHE 2.0") {
+    return (
+      "[![GitHub license](https://img.shields.io/badge/license-" +
+      license +
+      "-blue.svg)]"
+    );
+  } else if (license == '"BSD 2-Clause') {
+    return (
+      "[![GitHub license](https://img.shields.io/badge/license-" +
+      license +
+      "-orange.svg)]"
+    );
+  } else if (license == '"BSD 3-Clause') {
+    return (
+      "[![GitHub license](https://img.shields.io/badge/license-" +
+      license +
+      "-orange.svg)]"
+    );
+  } else if (license == "IPL") {
+    return (
+      "[![GitHub license](https://img.shields.io/badge/license-" +
+      license +
+      "-brightgreen.svg)]"
+    );
+  } else {
+    return " ";
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if(license=='GPL'){
-    return '(https://www.gnu.org/licenses/gpl-3.0)';
-  }else if(license=='MIT'){
-    return '(https://opensource.org/licenses/MIT)'; 
-  }else if(license=='APACHE'){
-    return '(https://opensource.org/licenses/Apache-2.0)'; 
-  }else if(license=='BSD'){
-    return '(https://opensource.org/licenses/BSD-3-Clause)'; 
-  }else if(license=='IPL'){
-    return '(https://opensource.org/licenses/IPL-1.0)'; 
-  }else{
-    return ' ';
+  if (license == "GPLv3") {
+    return "(https://www.gnu.org/licenses/gpl-3.0)";
+  } else if (license == "GPLv2") {
+    return "(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)";
+  } else if (license == "MIT") {
+    return "(https://opensource.org/licenses/MIT)";
+  } else if (license == "APACHE 2.0") {
+    return "(https://opensource.org/licenses/Artistic-2.0)";
+  } else if (license == '"BSD 2-Clause') {
+    return "(https://opensource.org/licenses/BSD-3-Clause)";
+  } else if (license == '"BSD 3-Clause') {
+    return "(https://opensource.org/licenses/BSD-2-Clause)";
+  } else if (license == "IPL") {
+    return "(https://opensource.org/licenses/IPL-1.0)";
+  } else {
+    return " ";
   }
-
 }
 
 // A function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
-
-  if(license!=='Other' || license!=="None"){
-    return 'This appliction is covered under ' + license + ' license';
-  }else{
-    return ''; 
+  if (license !== "Other" || license !== "None") {
+    return "This appliction is covered under " + license + " license";
+  } else {
+    return "";
   }
-  
-
 }
 
 // TODO: Create a function to generate markdown for README
@@ -65,7 +96,7 @@ function generateMarkdown(data) {
 
   1.) [Installation](#installation)
 
-  2,) [Usage](#usage)
+  2.) [Usage](#usage)
 
   3.) [Contribution guidelines](#contribution)
 
